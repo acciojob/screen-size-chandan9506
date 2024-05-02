feaaedfaf13d3b53f9617c753774750998eb9257
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-05-02 08:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let width = document.getElementById("width");
+let height = document.getElementById("height");
+
+function updateWidthAndHeight(){
+    var pageWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var pageHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+    width.innerText=`${pageWidth}`;
+    height.innerText=`${pageHeight}`;
+}
+
+updateWidthAndHeight();
+
+window.addEventListener("resize", updateWidthAndHeight);
+
+
+
